dad26f68318c007fbe91e255e3b197d767bbf6f1
tanraya <demerest@gmail.com>
2012-10-07 20:44:54 +0600

    Checking Rails version

--- lib/bootbox-rails.rb
+++ lib/bootbox-rails.rb
@@ -1,4 +1,5 @@
-require "bootbox-rails/engine"
-
-module BootboxRails
+if defined? Rails && Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 1
+  require "bootbox-rails/engine"
+else
+  puts "You should use Rails 3.1 and higher with bootbox-rails!"
 end
