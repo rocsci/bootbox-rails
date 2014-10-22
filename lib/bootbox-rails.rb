720dc355dbe78e6ffa1f378f11d2b70f52df708f
tanraya <demerest@gmail.com>
2014-10-22 22:26:53 +0600

    Make it work with rails 3.1+

--- lib/bootbox-rails.rb
+++ lib/bootbox-rails.rb
@@ -1,5 +1,5 @@
-if defined? Rails && Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 1
+if defined? Rails && Rails::VERSION::MAJOR >= 3 && Rails::VERSION::MINOR >= 1
   require "bootbox-rails/engine"
 else
-  puts "You should use Rails 3.1 and higher with bootbox-rails!"
+  puts "You should use Rails 3.1+ and higher with bootbox-rails!"
 end
